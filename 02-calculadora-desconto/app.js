78e2ab5a25f9db5f0b46e8f1135da44bf7ae06ac
ISRAEL FUJIMOTO <25203688@CFP127ALUNO.LOCAL>
2026-03-10 17:03:09 -0300

    Atividade 2 iniciada

---
+++ 02-calculadora-desconto/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function calcularDesconto(){
+    const preco = document.getElementById('preco')
+    const desconto = document.getElementById('desconto')
+    const resultado = document.getElementById('resultado') 
+
+    resultado = (preco * desconto) /100 
+
+}
+
+function valoreconomizado(){
+
+    
+
+
+}
